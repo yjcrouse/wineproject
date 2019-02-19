072c6820f48dee387877a830f853e294300814c6
Joe Crouse <yjcrouse@gmail.com>
2019-02-19 16:02:30 -0500

    jkc blue-project

---
+++ wineserver/db.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL,{
+     dialect: 'postgres'  
+});
+           
+sequelize.authenticate().then(
+    function() { 
+        console.log('Connected to workoutlog postgres database');
+    },
+    function(err){ 
+        console.log(err);
+    }
+);
+                 
+module.exports = sequelize;
